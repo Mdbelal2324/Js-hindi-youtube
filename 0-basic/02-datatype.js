87b9eaaba43a620a5ecd1e72cb6613ed0bf247a4
Md Belal <111410478+Mdbelal2324@users.noreply.github.com>
2024-08-01 07:04:46 +0000

    datatype in js

---
+++ 0-basic/02-datatype.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+// alert(3+3) // we are using nodejs not browser
+
+ console.log(3 
+    + 3) //code readability should be high
+
+console.log("hitesh")
+
+
+// javaScript me bahut saare data type hote hai jaise ki//
+//number     2 to the power 53;
+//bigint
+//string  ""
+//Boolean; true / false;
+//null stand alone value hai ye ek 
+//undefine
+//Symbol
+
+
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
