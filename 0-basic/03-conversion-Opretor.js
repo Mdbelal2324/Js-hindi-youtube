855dabd723c598a1228fd4855890316e263fb032
Md Belal <111410478+Mdbelal2324@users.noreply.github.com>
2024-08-01 07:04:19 +0000

    Conversion od datatype

---
+++ 0-basic/03-conversion-Opretor.js
@@ -0,0 +1,18 @@
+//let score = true;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+//let valueInNumber =Number(score);
+//console.log(score);
+
+let isLoggedIn =0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// note ye hai 0 write true and 1 write a false;
+
+// sting me ""=> false aayega
+//"belal"=> true aayega
