74151641be185660af857b496fdad3d30121529e
Md Belal <111410478+Mdbelal2324@users.noreply.github.com>
2024-08-02 05:43:53 +0000

    conversion

--- 0-basic/03-conversion-Opretor.js
+++ 0-basic/03-conversion-Opretor.js
@@ -7,12 +7,21 @@
 //let valueInNumber =Number(score);
 //console.log(score);
 
-let isLoggedIn =0
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let isLoggedIn =0
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 
 // note ye hai 0 write true and 1 write a false;
 
 // sting me ""=> false aayega
 //"belal"=> true aayega
+
+ // conversion conversion  ye hota hai number se boolean boolen se number nuber se string hota hai bas 
+
+
+
+ ///////************************** opretors************************************************ */
+ let value= 3;
+ let negValue= value;
+ console.log(negValue);
