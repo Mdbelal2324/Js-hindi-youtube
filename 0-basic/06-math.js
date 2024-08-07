ecf210f68842c4febd067839d846f49db912e98e
Md Belal <111410478+Mdbelal2324@users.noreply.github.com>
2024-08-07 11:20:52 +0000

    mathandNum

---
+++ 0-basic/06-math.js
@@ -0,0 +1,28 @@
+//***************** Number */
+
+
+
+const balance= new Number(100)
+//console.log(balance); 
+
+
+
+
+
+
+
+
+
+///////********* MAth  *****************************/
+//console.log(Math);
+//console.log(Math.abs(-4.9));
+//console.log(Math.round(9.8));
+//console.log(Math.ceil(3.1));
+//console.log(Math.floor(3.4));
+//console.log(Math.min(9,7,4,3));
+// console.log(Math.random());
+console.log((Math.random()*10) +1);
+
+
+
+
